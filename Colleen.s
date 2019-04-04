9cda35b11fb10628736966391388491e85c06dee
Lucas Guillerot lguiller <30439068+Lynux2142@users.noreply.github.com>
2019-04-04 10:43:24 +0200

    ajout de la fonction main

--- Colleen.s
+++ Colleen.s
@@ -1,10 +1,15 @@
+
 section .data
-s db "section .data%cs db %c%s%c,0%c%c ceci est un commentaire%csection .text%cglobal _start%cextern printf%c_start:%c%c ceci est un autre commentaire%center 0, 0%cmov rdi, s%cmov rsi, 10%cmov rdx, 34%cmov rcx, s%cmov r8, 34%cmov r9, 10%cmov rax, 27%cwhile:%ccmp rax, 0%cje endw%cdec rax%cpush 10%cjmp while%cendw:%cpush 59%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 59%ccall printf%cmov rax, 1%cmov rbx, 0%cint 0x80%c",0
+s db "section .data%cs db %c%s%c,0%c%c ceci est un commentaire%csection .text%cglobal _start%cglobal main%cextern printf%c_start:%ccall main%cret%cmain:%c%c ceci est un autre commentaire%center 0, 0%cmov rdi, s%cmov rsi, 10%cmov rdx, 34%cmov rcx, s%cmov r8, 34%cmov r9, 10%cmov rax, 31%cwhile:%ccmp rax, 0%cje endw%cdec rax%cpush 10%cjmp while%cendw:%cpush 59%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 59%ccall printf%cmov rax, 1%cmov rbx, 0%cint 0x80%c",0
 ; ceci est un commentaire
 section .text
 global _start
+global main
 extern printf
 _start:
+call main
+ret
+main:
 ; ceci est un autre commentaire
 enter 0, 0
 mov rdi, s
@@ -13,7 +18,7 @@ mov rdx, 34
 mov rcx, s
 mov r8, 34
 mov r9, 10
-mov rax, 27
+mov rax, 31
 while:
 cmp rax, 0
 je endw
@@ -22,6 +27,10 @@ push 10
 jmp while
 endw:
 push 59
+push 10
+push 10
+push 10
+push 10
 push 10
 push 10
 push 10
