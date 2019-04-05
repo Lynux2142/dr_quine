0ef6f70ffb41b7cc64381b504baf2ebd9a886f84
Lucas Guillerot lguiller <lguiller@student.42.fr>
2019-04-05 11:23:50 +0200

    ajout d'un makefile pour les programmes ASM et correction des erreurs de Colleen.s

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,14 +1,9 @@
-
 section .data
-s db "section .data%cs db %c%s%c,0%c%c ceci est un commentaire%csection .text%cglobal _start%cglobal main%cextern printf%c_start:%ccall main%cret%cmain:%c%c ceci est un autre commentaire%center 0, 0%cmov rdi, s%cmov rsi, 10%cmov rdx, 34%cmov rcx, s%cmov r8, 34%cmov r9, 10%cmov rax, 31%cwhile:%ccmp rax, 0%cje endw%cdec rax%cpush 10%cjmp while%cendw:%cpush 59%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 59%ccall printf%cmov rax, 1%cmov rbx, 0%cint 0x80%c",0
+s db "section .data%cs db %c%s%c,0%c%c ceci est un commentaire%csection .text%cglobal main%cextern printf%cmain:%c%c ceci est un autre commentaire%center 0, 0%cmov rdi, s%cmov rsi, 10%cmov rdx, 34%cmov rcx, s%cmov r8, 34%cmov r9, 10%cmov rax, 26%cwhile:%ccmp rax, 0%cje endw%cdec rax%cpush 10%cjmp while%cendw:%cpush 59%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 59%ccall printf%cleave%cret%c",0
 ; ceci est un commentaire
 section .text
-global _start
 global main
 extern printf
-_start:
-call main
-ret
 main:
 ; ceci est un autre commentaire
 enter 0, 0
@@ -18,7 +13,7 @@ mov rdx, 34
 mov rcx, s
 mov r8, 34
 mov r9, 10
-mov rax, 31
+mov rax, 26
 while:
 cmp rax, 0
 je endw
@@ -32,12 +27,7 @@ push 10
 push 10
 push 10
 push 10
-push 10
-push 10
-push 10
-push 10
 push 59
 call printf
-mov rax, 1
-mov rbx, 0
-int 0x80
+leave
+ret
