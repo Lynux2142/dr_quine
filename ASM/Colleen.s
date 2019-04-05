e4d4ae00ff0eac22bc30d1308fe6d045e5a2da22
Lucas Guillerot lguiller <lguiller@student.42.fr>
2019-04-05 12:12:50 +0200

    amelioration de Colleen.s

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,5 +1,5 @@
 section .data
-s db "section .data%cs db %c%s%c,0%c%c ceci est un commentaire%csection .text%cglobal main%cextern printf%cmain:%c%c ceci est un autre commentaire%center 0, 0%cmov rdi, s%cmov rsi, 10%cmov rdx, 34%cmov rcx, s%cmov r8, 34%cmov r9, 10%cmov rax, 26%cwhile:%ccmp rax, 0%cje endw%cdec rax%cpush 10%cjmp while%cendw:%cpush 59%cpush 10%cpush 10%cpush 10%cpush 10%cpush 10%cpush 59%ccall printf%cleave%cret%c",0
+s db "section .data%2$cs db %3$c%1$s%3$c,0%2$c%4$c ceci est un commentaire%2$csection .text%2$cglobal main%2$cextern printf%2$cmain:%2$c%4$c ceci est un autre commentaire%2$center 0, 0%2$cmov rdi, s%2$cmov rsi, s%2$cmov rdx, 10%2$cmov rcx, 34%2$cmov r8, 59%2$ccall printf%2$cleave%2$cret%2$c",0
 ; ceci est un commentaire
 section .text
 global main
@@ -8,26 +8,10 @@ main:
 ; ceci est un autre commentaire
 enter 0, 0
 mov rdi, s
-mov rsi, 10
-mov rdx, 34
-mov rcx, s
-mov r8, 34
-mov r9, 10
-mov rax, 26
-while:
-cmp rax, 0
-je endw
-dec rax
-push 10
-jmp while
-endw:
-push 59
-push 10
-push 10
-push 10
-push 10
-push 10
-push 59
+mov rsi, s
+mov rdx, 10
+mov rcx, 34
+mov r8, 59
 call printf
 leave
 ret
