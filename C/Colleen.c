d8d3a7852bc8706130fcb9573ad2e76b0794c17b
Lucas Guillerot <lguillerot@protego.net>
2019-04-05 10:42:10 +0200

    rajout de commentaire dans les programmes c et ajout de Colleen dans le dossier ASM

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+/*
+	ceci est un autre commentaire
+*/
+
 void display()
 {
-	const char *s = "#include <stdio.h>%c%cvoid display()%c{%c	const char *s = %c%s%c;%c	printf(s, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main(void)%c{%c	display();%c}%c";
-	printf(s, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10);
+	const char *s = "#include <stdio.h>%c%c%c*%c	ceci est un autre commentaire%c*%c%c%cvoid display()%c{%c	const char *s = %c%s%c;%c	printf(s, 10, 10, 47, 10, 10, 47, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 47, 10, 10, 47, 10, 10, 10);%c}%c%cint main(void)%c{%c	%c*%c		ceci est un commentaire%c	*%c%c	display();%c}%c";
+	printf(s, 10, 10, 47, 10, 10, 47, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 47, 10, 10, 47, 10, 10, 10);
 }
 
 int main(void)
 {
+	/*
+		ceci est un commentaire
+	*/
 	display();
 }
