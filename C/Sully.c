5c3c9e157756568853670392819d11d16d89ead3
Lucas GUILLEROT <lguiller@e1r11p9.42.fr>
2019-09-17 12:41:47 +0200

    modification de sully.c ajout de la creation de l'executable aillant le meme nom que le fichier genere

--- C/Sully.c
+++ C/Sully.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *concat(const char *s1, const char *s2, const char *s3)
+#define BEGIN_NAME		"Sully_"
+#define BEGIN_COMPILE	"gcc -Wall -Wextra -Werror "
+
+char *concat(const char *s1, const char *s2)
 {
 	char *str;
-	const int len = ((int)strlen(s1) + (int)strlen(s2) + (int)strlen(s3));
+	const size_t len = (strlen(s1) + strlen(s2));
 
 	str = (char*)malloc(sizeof(char) * (len + 1));
-	sprintf(str, "%s%s%s", s1, s2, s3);
+	sprintf(str, "%s%s", s1, s2);
 	return (str);
 }
 
@@ -18,18 +21,25 @@ int main(void)
 	const int i = 5;
 	char value[20];
 	sprintf(value, "%d", i - 1);
-	const char *child = concat("Sully_", value, ".c");
-	const char *s = "#include <stdio.h>%3$c#include <stdlib.h>%3$c#include <string.h>%3$c%3$cchar *concat(const char *s1, const char *s2, const char *s3)%3$c{%3$c	char *str;%3$c	const int len = ((int)strlen(s1) + (int)strlen(s2) + (int)strlen(s3));%3$c%3$c	str = (char*)malloc(sizeof(char) * (len + 1));%3$c	sprintf(str, %4$c%%s%%s%%s%4$c, s1, s2, s3);%3$c	return (str);%3$c}%3$c%3$cint main(void)%3$c{%3$c	FILE *file;%3$c	const int i = %2$s;%3$c	char value[20];%3$c	sprintf(value, %4$c%%d%4$c, i - 1);%3$c	const char *child = concat(%4$cSully_%4$c, value, %4$c.c%4$c);%3$c	const char *s = %4$c%1$s%4$c;%3$c%3$c	if (i > 0)%3$c	{%3$c		file = fopen(child, %4$cw+%4$c);%3$c		fprintf(file, s, s, value, 10, 34, 92);%3$c		fclose(file);%3$c		const char *test = concat(%4$cgcc -Wall -Wextra -Werror %4$c, child, %4$c -o Sully ; ./Sully%4$c);%3$c		system(test);%3$c		free((void*)test);%3$c	}%3$c	free((void*)child);%3$c	return (0);%3$c}%3$c";
+	const char *new_name = concat("Sully_", value);
+	const char *s = "#include <stdio.h>%3$c#include <stdlib.h>%3$c#include <string.h>%3$c%3$c#define BEGIN_NAME		%4$cSully_%4$c%3$c#define BEGIN_COMPILE	%4$cgcc -Wall -Wextra -Werror %4$c%3$c%3$cchar *concat(const char *s1, const char *s2)%3$c{%3$c	char *str;%3$c	const size_t len = (strlen(s1) + strlen(s2));%3$c%3$c	str = (char*)malloc(sizeof(char) * (len + 1));%3$c	sprintf(str, %4$c%%s%%s%4$c, s1, s2);%3$c	return (str);%3$c}%3$c%3$cint main(void)%3$c{%3$c	FILE *file;%3$c	const int i = %2$s;%3$c	char value[20];%3$c	sprintf(value, %4$c%%d%4$c, i - 1);%3$c	const char *new_name = concat(%4$cSully_%4$c, value);%3$c	const char *s = %4$c%1$s%4$c;%3$c%3$c	if (i > 0)%3$c	{%3$c		const char *new_file_name = concat(new_name, %4$c.c%4$c);%3$c		file = fopen(new_file_name, %4$cw+%4$c);%3$c		fprintf(file, s, s, value, 10, 34, 92);%3$c		fclose(file);%3$c		char *compile;%3$c		compile = (char*)malloc(sizeof(char) * (1 + strlen(BEGIN_COMPILE) + strlen(new_file_name) + strlen(%4$c -o %4$c) + strlen(new_name)));%3$c		sprintf(compile, %4$c%%s%%s%%s%%s%4$c, BEGIN_COMPILE, new_file_name, %4$c -o %4$c, new_name);%3$c		const char *exec = concat(%4$c./%4$c, new_name);%3$c		system(compile);%3$c		system(exec);%3$c		free((void*)new_file_name);%3$c		free((void*)compile);%3$c		free((void*)exec);%3$c	}%3$c	free((void*)new_name);%3$c	return (0);%3$c}%3$c";
 
 	if (i > 0)
 	{
-		file = fopen(child, "w+");
+		const char *new_file_name = concat(new_name, ".c");
+		file = fopen(new_file_name, "w+");
 		fprintf(file, s, s, value, 10, 34, 92);
 		fclose(file);
-		const char *test = concat("gcc -Wall -Wextra -Werror ", child, " -o Sully ; ./Sully");
-		system(test);
-		free((void*)test);
+		char *compile;
+		compile = (char*)malloc(sizeof(char) * (1 + strlen(BEGIN_COMPILE) + strlen(new_file_name) + strlen(" -o ") + strlen(new_name)));
+		sprintf(compile, "%s%s%s%s", BEGIN_COMPILE, new_file_name, " -o ", new_name);
+		const char *exec = concat("./", new_name);
+		system(compile);
+		system(exec);
+		free((void*)new_file_name);
+		free((void*)compile);
+		free((void*)exec);
 	}
-	free((void*)child);
+	free((void*)new_name);
 	return (0);
 }
