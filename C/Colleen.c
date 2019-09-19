fe5b624fbd6882e2df1ddb9711d9f591b068693f
Lucas GUILLEROT <lguiller@e1r10p7.42.fr>
2019-09-19 10:44:19 +0200

    ajout du return(0) a la fin du main colleen.c

--- C/Colleen.c
+++ C/Colleen.c
@@ -6,7 +6,7 @@
 
 void display()
 {
-	const char *s = "#include <stdio.h>%2$c%2$c%3$c*%2$c	ceci est un autre commentaire%2$c*%3$c%2$c%2$cvoid display()%2$c{%2$c	const char *s = %4$c%1$s%4$c;%2$c	printf(s, s, 10, 47, 34);%2$c}%2$c%2$cint main(void)%2$c{%2$c	%3$c*%2$c		ceci est un commentaire%2$c	*%3$c%2$c	display();%2$c}%2$c";
+	const char *s = "#include <stdio.h>%2$c%2$c%3$c*%2$c	ceci est un autre commentaire%2$c*%3$c%2$c%2$cvoid display()%2$c{%2$c	const char *s = %4$c%1$s%4$c;%2$c	printf(s, s, 10, 47, 34);%2$c}%2$c%2$cint main(void)%2$c{%2$c	%3$c*%2$c		ceci est un commentaire%2$c	*%3$c%2$c	display();%2$c	return (0);%2$c}%2$c";
 	printf(s, s, 10, 47, 34);
 }
 
@@ -16,4 +16,5 @@ int main(void)
 		ceci est un commentaire
 	*/
 	display();
+	return (0);
 }
